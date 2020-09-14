a7a428fe344e52867d69e030b5d86856e2473dc1
ABCF98 <acframewala_b16@ce.vjti.ac.in>
2020-09-14 21:16:32 +0530

    Mobile bugs fixed

--- main.js
+++ main.js
@@ -5,13 +5,18 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-
-  $('#one-tab').click();
-
+  $(document).ready(function(){
+    $('#one-tab').click();
+    $('#one-tab-mobile').click();
+  });
+  $('#one-tab-mobile').trigger( "click" );
   function openNavMobile() {
+    document.getElementById("mySidenav-mobile").style.paddingTop = "60px";
     document.getElementById("mySidenav-mobile").style.height = "100vh";
   }
   
   function closeNavMobile() {
     document.getElementById("mySidenav-mobile").style.height = "0";
+    document.getElementById("mySidenav-mobile").style.paddingTop = "0px";
+
   }
