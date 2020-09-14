ad3f6967dbcd35987cf3708f8e8fd6bc90b7e2d4
ABCF98 <acframewala_b16@ce.vjti.ac.in>
2020-09-14 20:27:26 +0530

    Menu Added

---
+++ main.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "20vw";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+
+  $('#one-tab').click();
+
+  function openNavMobile() {
+    document.getElementById("mySidenav-mobile").style.height = "100vh";
+  }
+  
+  function closeNavMobile() {
+    document.getElementById("mySidenav-mobile").style.height = "0";
+  }
